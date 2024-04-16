161f1506bd4be4a161d5ea17ac0fcb7adceebc97
Shiqi Yu <yushiqi815@gmail.com>
2024-04-15 21:32:43 -0500

    update PYTHONPATH to include utils

--- setup_combo_stable.sh
+++ setup_combo_stable.sh
@@ -2,15 +2,13 @@
 
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/setup.sh`
 module purge
-
+dir=pwd
 export PATH=/opt/software/core/lua/lua/bin:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/bin:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/bin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/usr/local/hpcc/bin:/usr/lpp/mmfs/bin:/opt/ibutils/bin:/opt/puppetlabs/bin
 export LD_LIBRARY_PATH=/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/lib:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/cernroot/lib:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/lib/tools:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/spack/opt/spack/linux-centos7-x86_64/gcc-9.2.0spack/geant4-10.04-jtcfrttq4krvbvabfjtkq7itsf3dieqg/lib64:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/lib:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/lib64:/tmp/tmpkvezwnnt/lib:/usr/lib64/nvidia
 export DYLD_LIBRARY_PATH=/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/lib:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/cernroot/lib:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/lib/tools:/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/spack/opt/spack/linux-centos7-x86_64/gcc-9.2.0spack/geant4-10.04-jtcfrttq4krvbvabfjtkq7itsf3dieqg/lib64
 export LIBRARY_PATH=
 export PYTHONPATH=/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/lib
-export PYTHONPATH=/mnt/home/yushiqi2/Analysis/LowEnergyNeuralNetwork/utils:$PYTHONPATH
-export PYTHONPATH=/mnt/research/IceCube/yushiqi2/IceCubeCNN:$PYTHONPATH
-export PYTHONPATH=/mnt/research/IceCube/yushiqi2/IceCubeCNN/data_pre:$PYTHONPATH
+export PYTHONPATH=$dir:$PYTHONPATH
 
 export I3_SRC=/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable
 export I3_BUILD=/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable
